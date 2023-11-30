f93da100a3e13aacf30df423cbb26938dfabbb1c
Ipinsokansunday <ipinboluwaji@yahoo.com>
2023-11-30 07:21:14 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import sys
 import hidden_4 as hidden
 
 if __name__ == "__main__":
-	for name in dir(hidden):
-		if not name.startswith("__"):
-			print(name)
+    for name in dir(hidden):
+    if not name.startswith("__"):
+        print(name)
