f4a44e4dc26fad35a287ceaaa435f16fc94617cf
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-01-09 08:58:11 -0800

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to a Python
+list, and then save them to a file"""
+
+
+import json
+
+
+def add_items_to_list_and_save(arguments):
+    """Adds command-line arguments to a Python list
+    and saves it as a JSON file.
+
+    Args:
+        arguments (list): List of command-line arguments.
+
+    Returns:
+        None
+    """
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
