6a3390dbdaaaa9dbfbee2d2654709d8cba5a1fbd
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-02-16 06:35:55 -0800

    3. Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Create the table 'force_name' in the 'hbtn_0d_2' database if it does not already exist
+-- creates a table in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
