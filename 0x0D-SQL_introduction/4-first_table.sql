954b71de71c6ca4bf9a1ba6c33666b7f3ee6ba4a
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-02-15 18:30:45 -0800

    4. First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---write a table called first_table with values id and name in my MySQL server.
+--creates a table called first_table with values id and name in my MySQL server.
 CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
