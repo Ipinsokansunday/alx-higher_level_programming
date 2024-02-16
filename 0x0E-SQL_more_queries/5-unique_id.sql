9d2f03c1c5a844dd5f43fe90f3f0a251f129630d
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-02-16 06:43:24 -0800

    5. Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,6 @@
 -- Create the table 'force_name' in the specified database if it does not already exist
 -- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
