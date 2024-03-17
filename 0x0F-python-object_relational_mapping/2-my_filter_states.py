676ad00822ba1d3cf2c12b50e360fc01f8249848
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-03-17 08:37:58 -0700

    2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Lists states from the database hbtn_0e_0_usa based on a provided name pattern."""
+
+import MySQLdb
+import sys
+
+def filter_states(db_user, db_password, db_name, name_pattern):
+    """Filter states based on the provided name pattern."""
+    try:
+        db = MySQLdb.connect(host="localhost", user=db_user,
+                             passwd=db_password, db=db_name, port=3306)
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+        cur.execute(query, (name_pattern,))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        if 'cur' in locals():
+            cur.close()
+        if 'db' in locals():
+            db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python3 my_filter_states.py [DB_USER] [DB_PASSWORD] [DB_NAME] [NAME_PATTERN]")
+        sys.exit(1)
+
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    name_pattern = sys.argv[4]
+
+    filter_states(db_user, db_password, db_name, name_pattern)
