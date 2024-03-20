a7fe4777f3429f1ffc390e53ed22c7a4a7f9457c
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-03-20 10:23:57 -0700

    9. Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""Script that lists all State objects that contain the letter a
-from the database hbtn_0e_6_usa"""
-
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Define the connection parameters
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create the connection string
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database))
-
-    # Bind the engine to the metadata of the Base class
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # Create a session maker instance
     Session = sessionmaker(bind=engine)
-
-    # Create a session
     session = Session()
-
-    # Query State objects containing the letter 'a' and sort by states.id
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-
-    # Print the results
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
-
-    # Close the session
-    session.close()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
