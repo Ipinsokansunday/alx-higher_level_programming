93125885b93aa37f2795b97a3499d3bfd8cfd036
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-03-27 05:08:16 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the URL and get the size of the response body in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
